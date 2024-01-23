045424cd38a7e3ddf660872c58b7f0d7c767954b
programmer-1991 <>
2024-01-23 21:39:53 +0000

    Migrate my database models to my new database and create superuser for it

--- tech_store/settings.py
+++ tech_store/settings.py
@@ -112,8 +112,13 @@ WSGI_APPLICATION = 'tech_store.wsgi.application'
 }"""
 
 DATABASES = {
-    'default': dj_database_url.parse('your-database-url-here')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
