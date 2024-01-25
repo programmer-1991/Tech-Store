377c253367d55bee5058aa4162339f1a59426d3c
programmer-1991 <>
2024-01-25 22:57:19 +0000

    Update SECRET_KEY and DEBUG variables

--- tech_store/settings.py
+++ tech_store/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['tech-store-dd2f1f0f1bd6.herokuapp.com', '8000-programmer-1991-tech-sto-vn9ghx5gto.us2.codeanyapp.com']
+ALLOWED_HOSTS = [
+'tech-store-dd2f1f0f1bd6.herokuapp.com',
+'8000-programmer-1991-tech-sto-vn9ghx5gto.us2.codeanyapp.com']
 
 
 # Application definition
