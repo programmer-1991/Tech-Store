7ab69d92cea171546823a5b8e94a18bbb225324d
programmer-1991 <>
2024-01-25 01:06:38 +0000

    Enable debug, set SECRET_KEY and add a host to ALLOWED_HOSTS

--- tech_store/settings.py
+++ tech_store/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['tech-store.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['tech-store.herokuapp.com', '8000-programmer-1991-tech-sto-vn9ghx5gto.us2.codeanyapp.com']
 
 
 # Application definition
